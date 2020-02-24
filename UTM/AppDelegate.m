75fd564b18c15f0d87555439daa36c952bfc4c91
unorderedpizza <43310440+unorderedpizza@users.noreply.github.com>
2020-02-25 07:35:43 +0800

    Assume user is quitting and terminate in switcher

--- UTM/AppDelegate.m
+++ UTM/AppDelegate.m
@@ -32,6 +32,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    exit(0);
 }
 
 
@@ -53,7 +54,6 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // You can't fire me, I quit!
-    exit(0);
 }
 
 
