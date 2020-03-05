2bb6fa40410b0b9be3a7e74f9ad79b6e2181bf68
osy <osy86@users.noreply.github.com>
2020-03-05 14:29:55 -0800

    vm: upcall for didReceiveMemoryWarning

--- Views/VMDisplayMetalViewController.m
+++ Views/VMDisplayMetalViewController.m
@@ -538,6 +538,7 @@ static CGFloat CGPointToPixel(CGFloat point) {
 #pragma mark - Memory warning
 
 - (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
     [self showAlert:NSLocalizedString(@"Running low on memory! UTM might soon be killed by iOS. You can prevent this by decreasing the amount of memory and/or JIT cache assigned to this VM", @"Low memory warning") completion:nil];
 }
 
