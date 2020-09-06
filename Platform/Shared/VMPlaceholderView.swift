b950de39d4d1748e99830a5f9fc51eb6f49b23dd
osy <osy86@users.noreply.github.com>
2020-09-05 19:52:37 -0700

    iOS+macOS: implement placeholder view

--- Platform/Shared/VMPlaceholderView.swift
+++ Platform/Shared/VMPlaceholderView.swift
@@ -18,17 +18,32 @@ import SwiftUI
 
 @available(iOS 14, macOS 11, *)
 struct VMPlaceholderView: View {
+    @Binding var createNewVMPresented: Bool
+    @Environment(\.openURL) private var openURL
+    
     var body: some View {
         VStack {
             Spacer()
             HStack {
                 Spacer()
-                Text("Welcome to UTM!").font(.headline)
+                Text("Welcome to UTM").font(.title)
                 Spacer()
             }
             HStack {
                 Spacer()
-                Text("Placeholder Here").font(.subheadline)
+                TileButton(titleKey: "Create a New Virtual Machine", systemImage: "plus.circle") {
+                    createNewVMPresented.toggle()
+                }
+                TileButton(titleKey: "Browse UTM Gallery", systemImage: "square.grid.3x2") {
+                    openURL(URL(string: "https://getutm.app/gallery/")!)
+                }
+                TileButton(titleKey: "User Guide", systemImage: "questionmark.circle") {
+                    #if os(macOS)
+                    openURL(URL(string: "https://mac.getutm.app/guide/")!)
+                    #else
+                    openURL(URL(string: "https://getutm.app/guide_v2/")!)
+                    #endif
+                }
                 Spacer()
             }
             Spacer()
@@ -37,8 +52,52 @@ struct VMPlaceholderView: View {
 }
 
 @available(iOS 14, macOS 11, *)
+private struct TileButton: View {
+    let titleKey: LocalizedStringKey
+    let systemImage: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Label(titleKey, systemImage: systemImage)
+                .labelStyle(TileLabelStyle())
+        }).buttonStyle(TileButtonStyle())
+    }
+}
+
+@available(iOS 14, macOS 11, *)
+private struct TileButtonStyle: ButtonStyle {
+    let defaultColor = Color(red: 220.0/255.0, green: 220.0/255.0, blue: 220.0/255.0)
+    let pressedColor = Color(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0)
+    let foregroundColor = Color(red: 126.0/255.0, green: 126.0/255.0, blue: 126.0/255.0)
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(configuration.isPressed ? pressedColor : defaultColor)
+            configuration.label
+                .foregroundColor(foregroundColor)
+        }.frame(width: 150, height: 150)
+    }
+}
+
+
+@available(iOS 14, macOS 11, *)
+private struct TileLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            configuration.icon
+                .font(.system(size: 48.0, weight: .medium))
+                .padding(.bottom)
+            configuration.title
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+@available(iOS 14, macOS 11, *)
 struct VMPlaceholderView_Previews: PreviewProvider {
     static var previews: some View {
-        VMPlaceholderView()
+        VMPlaceholderView(createNewVMPresented: .constant(false))
     }
 }
