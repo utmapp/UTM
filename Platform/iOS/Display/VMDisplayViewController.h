95372053d39b72a49ae72352c21af12820c37080
osy <osy86@users.noreply.github.com>
2020-09-07 14:04:20 -0700

    display: enable/disable toolbar buttons on context

--- Platform/iOS/Display/VMDisplayViewController.h
+++ Platform/iOS/Display/VMDisplayViewController.h
@@ -34,6 +34,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UIButton *pauseResumeButton;
 @property (weak, nonatomic) IBOutlet UIButton *restartButton;
 @property (weak, nonatomic) IBOutlet UIButton *zoomButton;
+@property (weak, nonatomic) IBOutlet UIButton *keyboardButton;
 @property (weak, nonatomic) IBOutlet UIButton *drivesButton;
 @property (weak, nonatomic) IBOutlet UIVisualEffectView *placeholderView;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *placeholderIndicator;
