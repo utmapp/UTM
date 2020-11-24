cf16e63f52e9690c4735c677c2716e294e0dfe9f
osy <osy86@users.noreply.github.com>
2020-11-24 14:32:50 -0600

    manager: reimplemented UTMLogging with Foundation methods

--- Managers/UTMLogging.m
+++ Managers/UTMLogging.m
@@ -19,7 +19,6 @@
 #import <unistd.h>
 #import "UTMLogging.h"
 
-static const int kLogBufferSize = 4096;
 static UTMLogging *gLoggingInstance;
 
 void UTMLog(NSString *format, ...) {
@@ -33,56 +32,22 @@ void UTMLog(NSString *format, ...) {
 
 @interface UTMLogging ()
 
+@property (nonatomic, readwrite) NSPipe *standardOutput;
+@property (nonatomic, readwrite) NSPipe *standardError;
 @property (nonatomic, nullable) NSOutputStream *fileOutputStream;
-
-- (void)didRecieveNewLine:(NSString *)line onDescriptor:(int)fd;
+@property (nonatomic, nullable) NSFileHandle *originalStdoutWrite;
+@property (nonatomic, nullable) NSFileHandle *originalStderrWrite;
 
 @end
 
-@implementation UTMLogging {
-    pthread_t _stdout_thread;
-    pthread_t _stderr_thread;
-    int _real_stdout;
-    int _real_stderr;
-    int _stdout;
-    int _stderr;
-}
-
-void *utm_logging_thread_stdout(void *arg) {
-    UTMLogging *self = (__bridge_transfer UTMLogging *)arg;
-    char ch;
-    NSMutableData *data = [NSMutableData dataWithCapacity:kLogBufferSize];
-    while (read(self->_stdout, &ch, 1) > 0) {
-        [data appendBytes:&ch length:1];
-        if (ch == '\n') {
-            NSString *line = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-            data = [NSMutableData dataWithCapacity:kLogBufferSize];
-            [self didRecieveNewLine:line onDescriptor:self->_stdout];
-        }
-    }
-    return NULL;
-}
-
-void *utm_logging_thread_stderr(void *arg) {
-    UTMLogging *self = (__bridge_transfer UTMLogging *)arg;
-    char ch;
-    NSMutableData *data = [NSMutableData dataWithCapacity:kLogBufferSize];
-    while (read(self->_stderr, &ch, 1) > 0) {
-        [data appendBytes:&ch length:1];
-        if (ch == '\n') {
-            NSString *line = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-            data = [NSMutableData dataWithCapacity:kLogBufferSize];
-            [self didRecieveNewLine:line onDescriptor:self->_stderr];
-        }
-    }
-    return NULL;
-}
+@implementation UTMLogging
 
 + (void)initialize {
     static BOOL initialized = NO;
     if (!initialized) {
         initialized = YES;
-        gLoggingInstance = [[UTMLogging alloc] initGlobal];
+        gLoggingInstance = [[UTMLogging alloc] init];
+        [gLoggingInstance redirectStandardFds];
     }
 }
 
@@ -90,125 +55,69 @@ void *utm_logging_thread_stderr(void *arg) {
     return gLoggingInstance;
 }
 
-- (instancetype)initGlobal {
-    self = [super init];
-    if (self) {
-        if (gLoggingInstance != nil) {
-            UTMLog(@"Trying to init more than one instance of UTMLogging!");
-            return nil;
-        }
-        int success = 0;
-        int new_stdout[2];
-        int new_stderr[2];
-        _stdout = _stderr = -1;
-        _real_stdout = _real_stderr = -1;
-        new_stdout[0] = new_stdout[1] = -1;
-        new_stderr[0] = new_stderr[1] = -1;
-        do {
-            if ((_real_stdout = dup(STDOUT_FILENO)) < 0) {
-                perror("dup");
-                break;
-            }
-            if ((_real_stderr = dup(STDERR_FILENO)) < 0) {
-                perror("dup");
-                break;
-            }
-            if (pipe(new_stdout) < 0) {
-                perror("pipe");
-                break;
-            }
-            if (pipe(new_stderr) < 0) {
-                perror("pipe");
-                break;
-            }
-            if (dup2(new_stdout[1], STDOUT_FILENO) < 0) {
-                perror("dup2");
-                break;
-            }
-            if (dup2(new_stderr[1], STDERR_FILENO) < 0) {
-                perror("dup2");
-                break;
-            }
-            if (close(new_stdout[1]) < 0) {
-                perror("close");
-                break;
-            }
-            new_stdout[1] = -1;
-            if (close(new_stderr[1]) < 0) {
-                perror("close");
-                break;
-            }
-            new_stderr[1] = -1;
-            _stdout = new_stdout[0];
-            _stderr = new_stderr[0];
-            if (pthread_create(&_stdout_thread, NULL, utm_logging_thread_stdout, (__bridge_retained void *)self) < 0) {
-                perror("pthread_create");
-                break;
-            }
-            if (pthread_create(&_stderr_thread, NULL, utm_logging_thread_stderr, (__bridge_retained void *)self) < 0) {
-                perror("pthread_create");
-                break;
-            }
-            success = 1;
-        } while (0);
-        if (!success) {
-            close(new_stdout[0]);
-            close(new_stdout[1]);
-            close(new_stderr[0]);
-            close(new_stderr[1]);
-            [self cleanup];
-            self = nil;
-        }
+- (instancetype)init {
+    if (self = [super init]) {
+        __weak typeof(self) _weakSelf = self;
+        self.standardOutput = [NSPipe pipe];
+        self.standardOutput.fileHandleForReading.readabilityHandler = ^(NSFileHandle *handle) {
+            typeof(self) _self = _weakSelf;
+            NSData *data = [handle availableData];
+            [_self.originalStdoutWrite writeData:data];
+            [_self.fileOutputStream write:data.bytes maxLength:data.length];
+        };
+        self.standardError = [NSPipe pipe];
+        __block NSString *errorBuffer = [NSString string];
+        self.standardError.fileHandleForReading.readabilityHandler = ^(NSFileHandle *handle) {
+            typeof(self) _self = _weakSelf;
+            NSData *data = [handle availableData];
+            [_self.originalStderrWrite writeData:data];
+            [_self.fileOutputStream write:data.bytes maxLength:data.length];
+            _self.lastErrorLine = [_self parseLastLine:data buffer:&errorBuffer];
+        };
     }
     return self;
 }
 
-- (void)cleanup {
-    if (_real_stdout != -1) {
-        dup2(_real_stdout, STDOUT_FILENO);
-        close(_real_stdout);
-        _real_stdout = -1;
+- (BOOL)redirectStandardFds {
+    int real_stdout = -1;
+    int real_stderr = -1;
+    if ((real_stdout = dup(STDOUT_FILENO)) < 0) {
+        perror("dup");
+        goto error;
     }
-    if (_real_stderr != -1) {
-        dup2(_real_stderr, STDERR_FILENO);
-        close(_real_stderr);
-        _real_stderr = -1;
+    if ((real_stderr = dup(STDERR_FILENO)) < 0) {
+        perror("dup");
+        goto error;
     }
-    if (_stdout != -1) {
-        close(_stdout);
-        _stdout = -1;
+    if (dup2(self.standardOutput.fileHandleForWriting.fileDescriptor, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        goto error;
     }
-    if (_stderr != -1) {
-        close(_stderr);
-        _stderr = -1;
+    if (dup2(self.standardError.fileHandleForWriting.fileDescriptor, STDERR_FILENO) < 0) {
+        perror("dup2");
+        goto error;
     }
-    if (_stdout_thread != NULL) {
-        void *res;
-        pthread_join(_stdout_thread, &res);
-        _stdout_thread = NULL;
-    }
-    if (_stderr_thread != NULL) {
-        void *res;
-        pthread_join(_stderr_thread, &res);
-        _stderr_thread = NULL;
-    }
-    [self endLog];
+    [self.standardOutput.fileHandleForWriting closeFile];
+    [self.standardError.fileHandleForWriting closeFile];
+    self.originalStdoutWrite = [[NSFileHandle alloc] initWithFileDescriptor:real_stdout closeOnDealloc:YES];
+    self.originalStderrWrite = [[NSFileHandle alloc] initWithFileDescriptor:real_stderr closeOnDealloc:YES];
+    return YES;
+error:
+    close(real_stdout);
+    close(real_stderr);
+    self.originalStdoutWrite = nil;
+    self.originalStderrWrite = nil;
+    return NO;
 }
 
-- (void)dealloc {
-    [self cleanup];
-}
-
-- (void)didRecieveNewLine:(NSString *)line onDescriptor:(int)fd {
-    if (fd == _stdout) {
-        write(_real_stdout, [line cStringUsingEncoding:NSASCIIStringEncoding], line.length);
-    } else if (fd == _stderr) {
-        self.lastErrorLine = line;
-        write(_real_stderr, [line cStringUsingEncoding:NSASCIIStringEncoding], line.length);
-    } else {
-        NSAssert(0, @"Invalid descriptor %d", fd);
+- (NSString *)parseLastLine:(NSData *)data buffer:(NSString **)buffer {
+    NSString *string = [*buffer stringByAppendingString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
+    NSArray *lines = [string componentsSeparatedByString:@"\n"];
+    *buffer = [lines lastObject];
+    if (lines.count > 0) {
+        lines = [lines subarrayWithRange:NSMakeRange(0, lines.count - 1)];
     }
-    [self writeLine:line];
+    return [lines lastObject];
 }
 
 - (void)logToFile:(NSURL *)path {
