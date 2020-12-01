3dc86ef0e8906caaa272ce422b45a327e2920385
osy <osy86@users.noreply.github.com>
2020-11-30 18:58:07 -0600

    build: support no-sandbox build for macOS

--- scripts/resign_mac.sh
+++ scripts/resign_mac.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 set -e
-if [ $# -ne 3 ]; then
-	echo "usage: $0 UTM.xcarchive outputPath TEAM_ID"
+if [ $# -lt 3 ]; then
+	echo "usage: $0 [--no-sandbox] UTM.xcarchive outputPath TEAM_ID"
 	exit 1
+fi
+
+NO_SANDBOX=0
+if [ "$1" == "--no-sandbox" ]; then
+	NO_SANDBOX=1
+	shift
 fi
 
 INPUT=$1
@@ -33,7 +39,49 @@ cat >"$OPTIONS" <<EOL
 </plist>
 EOL
 
-xcodebuild -exportArchive -exportOptionsPlist "$OPTIONS" -archivePath "$INPUT" -exportPath "$SIGNED"
-rm "$OPTIONS"
+# FIXME: until macOS Hypervisor.framework works with sandboxed NSTask, we have to remove it
+if [ $NO_SANDBOX -eq 1 ]; then
+	INPUT_COPY="/tmp/UTM.xcarchive"
+	QEMU_ENTITLEMENTS="/tmp/qemu.entitlements"
+	EMPTY_ENTITLEMENTS="/tmp/empty.entitlements"
 
+	cat >"$QEMU_ENTITLEMENTS" <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>com.apple.security.cs.allow-jit</key>
+	<true/>
+	<key>com.apple.security.hypervisor</key>
+	<true/>
+</dict>
+</plist>
+EOL
+
+	cat >"$EMPTY_ENTITLEMENTS" <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict/>
+</plist>
+EOL
+
+	rm -rf "$INPUT_COPY"
+	cp -r "$INPUT" "$INPUT_COPY"
+	find "$INPUT_COPY/Products/Applications/UTM.app" -path '*/qemu-system-*' -exec codesign --force --sign - --entitlements "$QEMU_ENTITLEMENTS" --timestamp=none --options runtime \{\} \;
+	codesign --force --sign - --entitlements "$EMPTY_ENTITLEMENTS" --timestamp=none --options runtime "$INPUT_COPY/Products/Applications/UTM.app/Contents/MacOS/UTM"
+	codesign --force --sign - --entitlements "$EMPTY_ENTITLEMENTS" --timestamp=none --options runtime "$INPUT_COPY/Products/Applications/UTM.app/Contents/XPCServices/QEMUHelper.xpc/Contents/MacOS/QEMUHelper"
+	rm "$QEMU_ENTITLEMENTS"
+	rm "$EMPTY_ENTITLEMENTS"
+	INPUT="$INPUT_COPY"
+fi
+
+xcodebuild -exportArchive -exportOptionsPlist "$OPTIONS" -archivePath "$INPUT" -exportPath "$SIGNED"
+
+rm "$OPTIONS"
+if [ ! -z "$INPUT_COPY" ]; then
+	rm -rf "$INPUT_COPY"
+fi
+
+rm -f "$OUTPUT/UTM.dmg"
 hdiutil create -fs HFS+ -srcfolder "$SIGNED/UTM.app" -volname "UTM" "$OUTPUT/UTM.dmg"
