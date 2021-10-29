3075268f53339cef7ac98645facc0c46827fc474
osy <50960678+osy@users.noreply.github.com>
2021-10-28 19:53:00 -0700

    savepanel: fix crash when window is nil
    Fixes #3208

--- Platform/macOS/SavePanel.swift
+++ Platform/macOS/SavePanel.swift
@@ -33,6 +33,10 @@ struct SavePanel: NSViewRepresentable {
                 return
             }
             
+            guard let window = nsView.window else {
+                return
+            }
+            
             // Initializing the SavePanel and setting it's properties
             let savePanel = NSSavePanel()
             savePanel.directoryURL = URL(fileURLWithPath: "/Users/\(NSUserName())/Downloads")
@@ -55,7 +59,7 @@ struct SavePanel: NSViewRepresentable {
             // Calling savePanel.begin with the appropriate completion handlers
             switch shareItem {
             case .debugLog(let sourceUrl), .utmVm(let sourceUrl):
-                savePanel.beginSheetModal(for: nsView.window!) { result in
+                savePanel.beginSheetModal(for: window) { result in
                     if result == .OK {
                         if let destUrl = savePanel.url {
                             do {
@@ -70,28 +74,30 @@ struct SavePanel: NSViewRepresentable {
                                 
                                 _ = try fileManager.replaceItemAt(destUrl, withItemAt: tempUrl)
                             } catch {
-                                data.alertMessage = AlertMessage(error.localizedDescription)
+                                DispatchQueue.main.async {
+                                    data.alertMessage = AlertMessage(error.localizedDescription)
+                                }
                             }
                         }
                     }
                 }
             case .qemuCommand(let command):
-                savePanel.beginSheetModal(for: nsView.window!) { result in
+                savePanel.beginSheetModal(for: window) { result in
                     if result == .OK {
                         if let destUrl = savePanel.url {
                             do {
                                 try command.write(to: destUrl, atomically: true, encoding: .utf8)
                             } catch {
-                                data.alertMessage = AlertMessage(error.localizedDescription)
+                                DispatchQueue.main.async {
+                                    data.alertMessage = AlertMessage(error.localizedDescription)
+                                }
                             }
                         }
                     }
                 }
             }
 
-            DispatchQueue.main.async {
-                isPresented = false
-            }
+            isPresented = false
         }
     }
 }
