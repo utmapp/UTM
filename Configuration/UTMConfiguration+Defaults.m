a2475288c0cc817921a55f89b2484499207d41ba
osy <50960678+osy@users.noreply.github.com>
2021-10-28 20:37:42 -0700

    config: reset default hardware devices based on target
    Also reset machine properties if empty.

--- Configuration/UTMConfiguration+Defaults.m
+++ Configuration/UTMConfiguration+Defaults.m
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+#import "UTMConfiguration+Constants.h"
 #import "UTMConfiguration+Defaults.h"
 #import "UTMConfiguration+Display.h"
 #import "UTMConfiguration+Miscellaneous.h"
@@ -40,6 +41,78 @@
     self.consoleFont = @"Menlo";
     self.consoleFontSize = @12;
     self.consoleTheme = @"Default";
+    self.networkCardMac = [UTMConfiguration generateMacAddress];
+    self.usbRedirectionMaximumDevices = @3;
+    self.name = [NSUUID UUID].UUIDString;
+    self.existingPath = nil;
+    self.selectedCustomIconPath = nil;
+}
+
+- (void)loadDisplayDefaultsForTarget:(nullable NSString *)target architecture:(nullable NSString *)architecture {
+    NSString *card = nil;
+    if ([target hasPrefix:@"pc"] || [target hasPrefix:@"q35"]) {
+        card = @"virtio-vga-gl";
+    } else if ([target isEqualToString:@"virt"] || [target hasPrefix:@"virt-"]) {
+        card = @"virtio-ramfb-gl";
+    } else if (architecture) {
+        NSArray<NSString *> *cards = [UTMConfiguration supportedDisplayCardsForArchitecture:architecture];
+        NSString *first = cards.firstObject;
+        if (first) {
+            card = first;
+        }
+    }
+    if (card.length == 0) {
+        self.displayCard = @"";
+        self.displayConsoleOnly = YES;
+        return;
+    }
+    self.displayCard = card;
+    self.displayConsoleOnly = NO;
+}
+
+- (void)loadSoundDefaultsForTarget:(nullable NSString *)target architecture:(nullable NSString *)architecture {
+    NSString *card = nil;
+    if ([target hasPrefix:@"pc"] || [target hasPrefix:@"q35"]) {
+        card = @"AC97";
+    } else if ([target isEqualToString:@"virt"] || [target hasPrefix:@"virt-"]) {
+        card = @"intel-hda";
+    } else if ([target isEqualToString:@"mac99"]) {
+        card = @"screamer";
+    } else if (architecture) {
+        NSArray<NSString *> *cards = [UTMConfiguration supportedSoundCardsForArchitecture:architecture];
+        NSString *first = cards.firstObject;
+        if (first) {
+            card = first;
+        }
+    }
+    if (card.length == 0) {
+        self.soundCard = @"";
+        self.soundEnabled = NO;
+        return;
+    }
+    self.soundCard = card;
+    self.soundEnabled = NO;
+}
+
+- (void)loadNetworkDefaultsForTarget:(nullable NSString *)target architecture:(nullable NSString *)architecture {
+    NSString *card = nil;
+    if ([target hasPrefix:@"pc"] || [target hasPrefix:@"q35"]) {
+        card = @"rtl8139";
+    } else if ([target isEqualToString:@"virt"] || [target hasPrefix:@"virt-"]) {
+        card = @"virtio-net-pci";
+    } else if (architecture) {
+        NSArray<NSString *> *cards = [UTMConfiguration supportedNetworkCardsForArchitecture:architecture];
+        NSString *first = cards.firstObject;
+        if (first) {
+            card = first;
+        }
+    }
+    if (card.length == 0) {
+        self.networkCard = @"";
+        self.networkMode = @"none";
+        return;
+    }
+    self.networkCard = card;
 #if TARGET_OS_OSX
     if (@available(macOS 11.3, *)) {
         self.networkMode = @"shared";
@@ -49,32 +122,19 @@
 #else
     self.networkMode = @"emulated";
 #endif
-    self.networkCardMac = [UTMConfiguration generateMacAddress];
-    self.usbRedirectionMaximumDevices = @3;
-    self.name = [NSUUID UUID].UUIDString;
-    self.existingPath = nil;
-    self.selectedCustomIconPath = nil;
 }
 
 - (void)loadDefaultsForTarget:(nullable NSString *)target architecture:(nullable NSString *)architecture {
+    [self loadDisplayDefaultsForTarget:target architecture:architecture];
+    [self loadSoundDefaultsForTarget:target architecture:architecture];
+    [self loadNetworkDefaultsForTarget:target architecture:architecture];
     if ([target hasPrefix:@"pc"] || [target hasPrefix:@"q35"]) {
-        self.soundCard = @"AC97";
-        self.soundEnabled = YES;
-        self.networkCard = @"rtl8139";
         self.shareClipboardEnabled = YES;
-        self.displayCard = @"virtio-vga-gl";
         self.systemBootUefi = YES;
     } else if ([target isEqualToString:@"virt"] || [target hasPrefix:@"virt-"]) {
-        self.soundCard = @"intel-hda";
-        self.soundEnabled = YES;
-        self.networkCard = @"virtio-net-pci";
         self.shareClipboardEnabled = YES;
-        self.displayCard = @"virtio-ramfb-gl";
         self.usb3Support = NO;
         self.systemBootUefi = YES;
-    } else if ([target isEqualToString:@"mac99"]) {
-        self.soundCard = @"screamer";
-        self.soundEnabled = YES;
     } else if ([target isEqualToString:@"isapc"]) {
         self.inputLegacy = YES; // no USB support
     } else {
@@ -83,6 +143,8 @@
     NSString *machineProp = [UTMConfiguration defaultMachinePropertiesForTarget:target];
     if (machineProp) {
         self.systemMachineProperties = machineProp;
+    } else if (self.systemMachineProperties) {
+        self.systemMachineProperties = @"";
     }
     if (target && architecture) {
         self.systemCPU = [UTMConfiguration defaultCPUForTarget:target architecture:architecture];
