d2f5c6a8cbec5b5967b0ad4621642a9f0127cf96
osy <50960678+osy@users.noreply.github.com>
2022-02-28 07:57:41 -0800

    build: thin Swift libraries to workaround crash on older iOS
    Fixes #3654

--- scripts/build_utm.sh
+++ scripts/build_utm.sh
@@ -87,6 +87,8 @@ fi
 
 xcodebuild archive -archivePath "$OUTPUT" -scheme "$SCHEME" -sdk "$SDK" $ARCH_ARGS -configuration Release CODE_SIGNING_ALLOWED=NO $TEAM_IDENTIFIER_PREFIX
 BUILT_PATH=$(find $OUTPUT.xcarchive -name '*.app' -type d | head -1)
+# remove unsupported architectures to address < iOS 15 crash
+find "$BUILT_PATH" -type f -path '*/Frameworks/*.dylib' -exec lipo -remove arm64e \{\} -output \{\} \;
 find "$BUILT_PATH" -type d -path '*/Frameworks/*.framework' -exec codesign --force --sign - --timestamp=none \{\} \;
 if [ "$PLATFORM" == "macos" ]; then
     # always build with vm entitlements, package_mac.sh can strip it later
