9ee6086931f19569e1aed36215e0f52082b4e522
osy <50960678+osy@users.noreply.github.com>
2022-04-24 15:20:49 -0700

    system: set "discard=unmap,detect-zeroes=unmap" on block devices
    Resolves #3460

--- Managers/UTMQemuSystem.m
+++ Managers/UTMQemuSystem.m
@@ -317,7 +317,7 @@ static size_t sysctl_read(const char *name) {
                 [self pushArgv:@"-drive"];
                 drive = [NSString stringWithFormat:@"if=%@,media=%@,id=%@", realInterface, (removable && !floppy) ? @"cdrom" : @"disk", identifier];
                 if (hasImage) {
-                    drive = [NSString stringWithFormat:@"%@,file=%@", drive, fullPathURL.path];
+                    drive = [NSString stringWithFormat:@"%@,file=%@,discard=unmap,detect-zeroes=unmap", drive, fullPathURL.path];
                 }
                 [self pushArgv:drive];
                 break;
