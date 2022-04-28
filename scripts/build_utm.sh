1852f93c3cf6dd82925d21c65eae8290d7e50fe3
Halo-Michael <45062120+Halo-Michael@users.noreply.github.com>
2022-04-28 16:41:31 +0800

    Fix for bash

--- scripts/build_utm.sh
+++ scripts/build_utm.sh
@@ -89,8 +89,8 @@ xcodebuild archive -archivePath "$OUTPUT" -scheme "$SCHEME" -sdk "$SDK" $ARCH_AR
 BUILT_PATH=$(find $OUTPUT.xcarchive -name '*.app' -type d | head -1)
 # remove unsupported architectures to address < iOS 15 crash
 if [[ ! $PLATFORM =~ simulator ]]; then
-    find "$BUILT_PATH" -type f -path '*/Frameworks/*.dylib' | while read FILE; do if [[ $(lipo -info "$FILE") =~ "Architectures in the fat file" ]] then lipo -thin $ARCH "$FILE" -output "$FILE"; fi; done
-    find "$BUILT_PATH" -type f -path '*/Frameworks/*.framework/*' ! -name "Info.plist" ! -name "CodeResources" | while read FILE; do if [[ $(file "$FILE") =~ Mach-O ]] && [[ $(lipo -info "$FILE") =~ "Architectures in the fat file" ]] then lipo -thin $ARCH "$FILE" -output "$FILE"; fi; done
+    find "$BUILT_PATH" -type f -path '*/Frameworks/*.dylib' | while read FILE; do if [[ $(lipo -info "$FILE") =~ "Architectures in the fat file" ]]; then lipo -thin $ARCH "$FILE" -output "$FILE"; fi; done
+    find "$BUILT_PATH" -type f -path '*/Frameworks/*.framework/*' ! -name "Info.plist" ! -name "CodeResources" | while read FILE; do if [[ $(file "$FILE") =~ Mach-O ]] && [[ $(lipo -info "$FILE") =~ "Architectures in the fat file" ]]; then lipo -thin $ARCH "$FILE" -output "$FILE"; fi; done
 fi
 find "$BUILT_PATH" -type d -path '*/Frameworks/*.framework' -exec codesign --force --sign - --timestamp=none \{\} \;
 if [ "$PLATFORM" == "macos" ]; then
