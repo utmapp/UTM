5cd7aab72c8a95300179405ce152952fb09cd289
osy <50960678+osy@users.noreply.github.com>
2022-12-11 22:00:24 -0800

    api: hide main window when launched by CLI

--- Platform/macOS/AppDelegate.swift
+++ Platform/macOS/AppDelegate.swift
@@ -27,6 +27,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return vmList.contains(where: { $0.state == .vmStarted || ($0.state == .vmPaused && !$0.hasSaveState) })
     }
     
+    private var isLaunchedByCLI: Bool {
+        if CommandLine.arguments.count > 1 && CommandLine.arguments[1] == "cli-request" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         !isKeepRunningAfterLastWindowClosed && !hasRunningVirtualMachines
     }
@@ -85,9 +93,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
-    func applicationWillFinishLaunching(_ notification: Notification) {
+    func applicationDidFinishLaunching(_ notification: Notification) {
         if isDockIconHidden {
             NSApp.setActivationPolicy(.accessory)
+        }
+        if isLaunchedByCLI && isKeepRunningAfterLastWindowClosed {
+            NSApp.windows.first?.close()
         }
     }
 }
