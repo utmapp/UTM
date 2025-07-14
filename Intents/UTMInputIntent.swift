185ce4f8b526fe45eb048a5f0b62fd7f57c5c605
osy <50960678+osy@users.noreply.github.com>
2025-07-14 01:37:40 -0700

    intents: add keystrokes intent
    Refactor VMKeyboardView so it can be used on macOS for converting
    a sequence of text to keyboard strokes.

--- Intents/UTMInputIntent.swift
+++ Intents/UTMInputIntent.swift
@@ -62,6 +62,97 @@ struct UTMSendScanCodeIntent: UTMIntent {
 }
 
 @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
+struct UTMSendKeystrokesIntent: UTMIntent {
+    static let title: LocalizedStringResource = "Send Keystrokes"
+    static let description = IntentDescription("Send text as a sequence of keystrokes to the virtual machine. Only supported on QEMU backend.")
+    static var parameterSummary: some ParameterSummary {
+        Summary("Send \(\.$keystrokes) to \(\.$vmEntity)") {
+            \.$modifiers
+        }
+    }
+
+    enum Modifier: Int, CaseIterable, AppEnum {
+        case capsLock
+        case shift
+        case control
+        case option
+        case command
+        case escape
+
+        static let typeDisplayRepresentation: TypeDisplayRepresentation =
+            TypeDisplayRepresentation(
+                name: "Modifier Key"
+            )
+
+        static let caseDisplayRepresentations: [Modifier: DisplayRepresentation] = [
+            .capsLock: DisplayRepresentation(title: "Caps Lock (⇪)"),
+            .shift: DisplayRepresentation(title: "Shift (⇧)"),
+            .control: DisplayRepresentation(title: "Control (⌃)"),
+            .option: DisplayRepresentation(title: "Option (⌥)"),
+            .command: DisplayRepresentation(title: "Command (⌘)"),
+            .escape: DisplayRepresentation(title: "Escape (⎋)"),
+        ]
+
+        func toSpiceKeyCode() -> Int32 {
+            switch self {
+            case .capsLock: return 0x3a
+            case .shift: return 0x2a
+            case .control: return 0x1d
+            case .option: return 0x38
+            case .command: return 0x15b
+            case .escape: return 0x01
+            }
+        }
+    }
+
+    @Dependency
+    var data: UTMData
+
+    @Parameter(title: "Virtual Machine", requestValueDialog: "Select a virtual machine")
+    var vmEntity: UTMVirtualMachineEntity
+
+    @Parameter(title: "Keystrokes", description: "Text will be converted to a sequnce of keystrokes.")
+    var keystrokes: String
+
+    @Parameter(title: "Modifiers", description: "The modifier keys will be held down while the keystroke sequence is sent.", default: [])
+    var modifiers: [Modifier]
+
+    @MainActor
+    func perform(with vm: any UTMVirtualMachine, boxed: VMData) async throws -> some IntentResult {
+        guard let vm = vm as? any UTMSpiceVirtualMachine else {
+            throw UTMIntentError.unsupportedBackend
+        }
+        guard let input = vm.ioService?.primaryInput else {
+            throw UTMIntentError.inputHandlerNotAvailable
+        }
+        for modifier in modifiers {
+            input.send(.press, code: modifier.toSpiceKeyCode())
+            try await Task.sleep(nanoseconds: kDelayNs)
+        }
+        let keyboardMap = VMKeyboardMap()
+        await keyboardMap.mapText(keystrokes) { scanCode in
+            input.send(.release, code: scanCodeToSpice(scanCode))
+        } keyDown: { scanCode in
+            input.send(.press, code: scanCodeToSpice(scanCode))
+        }
+        try await Task.sleep(nanoseconds: kDelayNs)
+        for modifier in modifiers {
+            input.send(.release, code: modifier.toSpiceKeyCode())
+            try await Task.sleep(nanoseconds: kDelayNs)
+        }
+        return .result()
+    }
+
+    private func scanCodeToSpice(_ scanCode: Int) -> Int32 {
+        var keyCode = scanCode
+        if (keyCode & 0xFF00) == 0xE000 {
+            keyCode = (keyCode & 0xFF) | 0x100
+        }
+        return Int32(keyCode)
+    }
+}
+
+@available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
 struct UTMMouseClickIntent: UTMIntent {
     static let title: LocalizedStringResource = "Send Mouse Click"
     static let description = IntentDescription("Send a mouse position and click to the virtual machine. Only supported on QEMU backend.")
